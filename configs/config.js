52fe722bd2c1519c5465fee195205d981fa2b1cf
Septia Permana <septiapermana@gmail.com>
2022-05-28 17:30:53 +0700

    update config

--- configs/config.js
+++ configs/config.js
@@ -1,7 +1,7 @@
 require("dotenv").config()
 const config = {}
 
-config.redis_address = process.env.REDIS_ADDRESS
-config.redis_port = process.env.REDIS_PORT
+config.redis_address = process.env.REDIS_ADDRESS ? process.env.REDIS_ADDRESS : '127.0.0.1'
+config.redis_port = process.env.REDIS_PORT ? process.env.REDIS_PORT : '6317' 
 
 module.exports = config
